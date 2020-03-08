e107a911a57b7c44f8a23729d0d40c7148496ec0
Roberto Díaz <robertodzbt@gmail.com>
2020-03-08 21:10:26 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './components/layout/header';
+import { Content } from './components/layout/Content';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
