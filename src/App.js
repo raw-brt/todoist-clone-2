d9b148246f1c52c90602d3ddee46f278d3ed065d
Roberto Díaz <robertodzbt@gmail.com>
2020-03-09 14:46:02 +0100

    Context for projects added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Header } from './components/layout/header';
 import { Content } from './components/layout/Content';
 import './App.scss';
-
+import { ProjectsProvider, SelectedProjectProvider } from './context'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+      </SelectedProjectProvider>
   );
 }
 
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -16,12 +16,18 @@ export const useTasks = selectedProject => {
       .where('userId', '==', 'kcGngUfl8ZJ65f');
 
       unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ?
+      
       (unsubscribe = unsubscribe.where('projectId', '==', selectedProject)) :
+      
       selectedProject === 'TODAY' ?
-      (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
-      : selectedProject === 'INBOX' || selectedProject === 0
-      ? (unsubscribe = unsubscribe.where('date', '==', ''))
-      : unsubscribe;
+     
+      (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY'))) : 
+      
+      selectedProject === 'INBOX' || selectedProject === 0 ? 
+      
+      (unsubscribe = unsubscribe.where('date', '==', '')) : 
+      
+      unsubscribe;
 
       unsubscribe = unsubscribe.onSnapshot(snapshot => {
         const newTasks = snapshot.docs.map(task => ({
