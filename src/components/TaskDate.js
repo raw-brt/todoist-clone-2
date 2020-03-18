72cebd810a673214f06f90277c739d66042c7100
Roberto Díaz <robertodzbt@gmail.com>
2020-03-18 16:12:36 +0100

    Some bugs fixed

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -2,49 +2,89 @@ import React from 'react';
 import moment from 'moment';
 import { FaSpaceShuttle, FaSun, FaRegPaperPlane } from 'react-icons/fa';
 
-export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => {
-  return(
-    showTaskDate && (
-      <div className='task-date' data-testid='task-date-overlay'>
-        <ul className='task-date__list'>
-          <li
+export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
+  showTaskDate && (
+    <div className="task-date" data-testid="task-date-overlay">
+      <ul className="task-date__list">
+        <li>
+          <div
             onClick={() => {
               setShowTaskDate(false);
               setTaskDate(moment().format('DD/MM/YYYY'));
             }}
-            data-testid='task-date-overlay'
+            onKeyDown={() => {
+              setShowTaskDate(false);
+              setTaskDate(moment().format('DD/MM/YYYY'));
+            }}
+            data-testid="task-date-today"
+            tabIndex={0}
+            aria-label="Select today as the task date"
+            role="button"
           >
             <span>
               <FaSpaceShuttle />
             </span>
             <span>Today</span>
-          </li>
-          <li
+          </div>
+        </li>
+        <li>
+          <div
             onClick={() => {
               setShowTaskDate(false);
-              setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
+              setTaskDate(
+                moment()
+                  .add(1, 'day')
+                  .format('DD/MM/YYYY')
+              );
             }}
-            data-testid='task-date-tomorrow'
+            onKeyDown={() => {
+              setShowTaskDate(false);
+              setTaskDate(
+                moment()
+                  .add(1, 'day')
+                  .format('DD/MM/YYYY')
+              );
+            }}
+            data-testid="task-date-tomorrow"
+            role="button"
+            tabIndex={0}
+            aria-label="Select tomorrow as the task date"
           >
             <span>
               <FaSun />
             </span>
             <span>Tomorrow</span>
-          </li>
-          <li
+          </div>
+        </li>
+        <li>
+          <div
             onClick={() => {
               setShowTaskDate(false);
-              setTaskDate(moment().add(7, 'day').format('DD/MM/YYYY'));
+              setTaskDate(
+                moment()
+                  .add(7, 'days')
+                  .format('DD/MM/YYYY')
+              );
             }}
-            data-testid='task-date-next-week'
+            onKeyDown={() => {
+              setShowTaskDate(false);
+              setTaskDate(
+                moment()
+                  .add(7, 'days')
+                  .format('DD/MM/YYYY')
+              );
+            }}
+            data-testid="task-date-next-week"
+            aria-label="Select next week as the task date"
+            tabIndex={0}
+            role="button"
           >
             <span>
               <FaRegPaperPlane />
             </span>
-            <span>Next Week</span>
-          </li>
-        </ul>
-      </div>
-    )
-  )
-}
+            <span>Next week</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
